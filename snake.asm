92616031d7e9c808f7437a48bb94a3c5c6ba1fc8
GEORGE RIZOS <81235981+grrizos@users.noreply.github.com>
2025-01-04 21:58:29 +0200

    Update snake.asm

--- snake.asm
+++ snake.asm
@@ -2,7 +2,7 @@
 
 data segment
     ; add your data here!
-
+     msg db "hello world $"
 ends
 
 stack segment
@@ -30,17 +30,14 @@ main_loop:
     
 LOOP1:  
  
-    call PRA 
-    call PGREENBOX
+    call PRA             ;call the functions in correct order
+    call PGREENBOX       ;and again and again in the LOOP1
     call MOVESNAKE
     call WAITFUNC
    
     jmp LOOP1
    
-   
-    
-jmp end
-    
+
     
     
     
@@ -67,19 +64,19 @@ again1:
     mov ah,02h    ; set cursor position.
     int 10h       ; BIOS interrupt  
     
-    mov ah,0Ah  
+    mov ah,0Ah    ;print the A representing the road 
     mov al,'A'
     mov bh,0
-    mov cx,28h
+    mov cx,27h
     int 10h   
     
-    pop cx
+    pop cx        ;use cx first print the upper road and then the lower one
     dec cx
     push cx
-    add dh,04h
-    cmp cx,0
+    add dh,04h    ;move down 4 rows
+    cmp cx,0      
     jne again1
-    pop cx 
+    pop cx        ;pop cx,we dont need it anymore
     
     ret
 
@@ -87,9 +84,9 @@ again1:
 PGREENBOX: 
 
     
-    mov cx,4d 
-    push cx
-    mov dl,34d
+    mov cx,4d     ;use cx to print the 4 shells for the greenbox
+    push cx       ;push it into stack
+    mov dl,34d    ;set cursor to desired column and row
     mov dh,1
 
 
@@ -98,8 +95,8 @@ PGREENBOX:
     mov ah,02h    ; set cursor position.
     int 10h       ; BIOS interrupt  
     
-    mov ah,09h  
-    mov al, ' '
+    mov ah,09h    ;call interupt using green color and nothing as text
+    mov al, ' '   ;we dont need any text
     mov bh,0
     mov bl,00100010b
     mov cx,05h
@@ -115,10 +112,10 @@ PGREENBOX:
     pop cx
     ret     
     
-PREDBOX: 
+PREDBOX:          ;everythink we did for the green box, just with red as atrribute
 
     
-    mov cx,4d 
+    mov cx,4d      
     push cx
     mov dl,34d
     mov dh,1
@@ -156,7 +153,8 @@ ADDB:
     pop cx        ; pop length on cx
    
     inc cx        ;inc length 
-
+    cmp cx,40     ;compare the lenght of the snake, if its 40 it means it is as big as the screen so the program ends
+    je end
 
     push cx       ; push length
     push dx       ; push ret of waitfunc
@@ -173,26 +171,32 @@ MOVESNAKE:
     push ax       ;push ret in stack, will pop later to add above it the length and location
     mov dh,10
   again5:
-    
+    cmp dl,40    ;check if the snake reached the end of the screen 
+    je restart    ;go to restart, that moves the snake in the begging of the screen
+    HERE:
     mov ah,02h    ; set cursor position.
     int 10h       ; BIOS interrupt  
     
-    mov ah,0Ah  
+    mov ah,0Ah    ;print the snake tail in the set by dx location
     mov al,'B'
     mov bh,0
 
     int 10h
    
 
-    inc dl   
+    inc dl        ;move snake into the next spot
     pop ax
-    push dx
+    push dx      
     push cx
     push ax
 
 
     ret
 
+restart:
+    mov dl,0
+    jmp HERE      +
+    
 
 WAITFUNC:
 
@@ -207,8 +211,9 @@ WAITFUNC:
     
     
     mov ah,86h       ;call interupt to wait for 0.1sec
-    mov cx, 0x01h
-    mov dx, 0x6A0h
+    mov cx, 0x0001h
+    mov dx, 0x86A0h
+
     
     int 15h
     
@@ -229,9 +234,7 @@ WAITFUNC:
     cmp bx,10d
     je printred
     return2:
-    cmp bx,20d
-    je printgreen
-    return3:
+
     
     
     
@@ -243,8 +246,9 @@ addbcall:
     call ADDB
     jmp return1  
     
-printgreen:
-    call PGREENBOX 
+    
+printred:
+    call PREDBOX 
     pop ax        ; pop ret
     pop cx        ; cx is the length
     pop dx        ; dx is the location of the tail   
@@ -255,15 +259,39 @@ printgreen:
     push bx          ;push everything in again while bx is the times called
     push dx
     push cx
-    push ax
-    jmp return2
+    push ax   
     
-printred:
-    call PREDBOX    
-    
-    jmp return3  
-     
-end:
-ends
+    jmp return2 
 
-end start ; set entry point and stop the assembler.
+
+
+     
+end: 
+	mov ah,05h
+	mov al,02h
+	int 10h
+	
+	
+	mov al, 1
+	mov bh, 2
+	mov bl, 0011_1011b
+	mov cx, msg1end - offset msg1 ; calculate message size. 
+	mov dl, 0
+	mov dh, 10
+	push cs
+	pop es
+	mov bp, offset msg1
+	mov ah, 13h
+	int 10h
+	jmp msg1end
+	msg1 db "Its not a bug , its an undocumented feature "
+	msg1end:  
+		
+		mov ah,86h       ;call interupt to wait for 0.1sec
+        mov cx, 0x0201h
+        mov dx, 0x86A0h
+
+    
+    int 15h
+
+
